c012cf7f6379af45a2a2a7a57d8e135fab615390
bulidiriba <bulidiriba@gmail.com>
2021-12-10 22:35:06 +0300

    get the latest list of tweet from timeline(20 by default)

--- tweepy_bots.py
+++ tweepy_bots.py
@@ -1,8 +1,8 @@
 import tweepy
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler("[API_KEY]", "[API_KEY_SECRET]")
-auth.set_access_token("[Access_Token]", "[Access_Token_Secret]")
+auth = tweepy.OAuthHandler("lsvCzTohCFFjNXIbHPcgqhQZh", "ECOvhM6Mde2SzNYcnRdRw2tgqwwM2mpR2wRe1fRkfyf3056mEg")
+auth.set_access_token("4107572963-JVpQTXOreSNGaBnxiXw88dKu8q6oZTm5sWgOwF9", "AtNLKoXCXOTkrosnj0Nt8zmqLTjg3i4P0iCXREA6GImDy")
 
 api = tweepy.API(auth)
 
@@ -12,3 +12,7 @@ try:
 except:
     print("Error during authentication")
 
+# To get the latest list of timeline(20 by default)
+timeline = api.home_timeline()
+for tweet in timeline:
+    print(f"{tweet.user.name} said {tweet.text}")
